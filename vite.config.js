9a39b893e4936e2b78e488cb54541ab79187f4fa
devitce <aupovtimur0@mail.ru>
2025-06-05 13:30:46 +0300

    Fix asset paths and routing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/notes-project/', 
+  base: '/notes-project/',
   plugins: [vue()],
   build: {
     outDir: 'dist',
@@ -12,7 +11,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': '/src'
     }
   }
 })
