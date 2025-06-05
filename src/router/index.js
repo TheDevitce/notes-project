25ea38a1e41ee5d2a3808dec98352f2339021563
devitce <aupovtimur0@mail.ru>
2025-06-05 13:55:46 +0300

    Add 404.html and fixng

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 import MainPage from '../views/MainPage.vue'
 import Notes from '../views/Notes.vue'
 import AllNotes from '../views/AllNotes.vue'
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory (),
   routes
 })
 
